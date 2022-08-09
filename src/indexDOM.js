ae5a9508384d2d66b4b8e55cf5d62e35cfd748f7
Adam Purcell <apurcell88@gmail.com>
2022-08-09 16:05:43 -0500

    Started styling. Added the date and time in JS.

--- src/indexDOM.js
+++ src/indexDOM.js
@@ -2,6 +2,8 @@ import Weather from './index';
 
 const clouds = document.querySelector('[data-clouds]');
 const city = document.querySelector('[data-city]');
+const date = document.querySelector('[data-date]');
+const time = document.querySelector('[data-time]');
 const temp = document.querySelector('[data-temp]');
 const feelsLike = document.querySelector('[data-feels-like]');
 const humidity = document.querySelector('[data-humidity]');
@@ -9,10 +11,11 @@ const wind = document.querySelector('[data-wind]');
 
 async function displayWeather() {
   const weatherInfo = await Weather();
-  // capitalize the first letter of each word for sky and location
 
   clouds.textContent = weatherInfo.createWeather().sky;
   city.textContent = weatherInfo.createWeather().location;
+  date.textContent = weatherInfo.createWeather().date;
+  time.textContent = weatherInfo.createWeather().time;
   temp.textContent = `${weatherInfo.createWeather().temp} F`;
   feelsLike.textContent = `${weatherInfo.createWeather().feelsLike} F`;
   humidity.textContent = weatherInfo.createWeather().humidity;
